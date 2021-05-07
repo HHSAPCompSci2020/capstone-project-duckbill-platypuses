d1b98e25b590ca19807a9f5992b618c18eb3732c
Ophir Peleg <opeleg820@student.fuhsd.org>
2021-05-07 14:40:04 -0700

    Update Problem.java

--- src/Problem.java
+++ src/Problem.java
@@ -1,7 +1,7 @@
 
 /**
- * The Problem class represents a problem that is given in a class,
- * which holds the question and answer of the problem.
+ * The Problem class represents a problem that is given in a class.
+ * The class holds the question and answer of the problem.
  *  * 
  * @author Ophir Peleg
  * @version 05/06/2021
