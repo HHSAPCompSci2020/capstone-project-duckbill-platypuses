e6332219670e3b52caaa65425fefd506bbdfafa8
Ophir Peleg <opeleg820@student.fuhsd.org>
2021-05-07 14:22:30 -0700

    Update DrawingSurface.java

--- src/DrawingSurface.java
+++ src/DrawingSurface.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import processing.core.PApplet;
 
 /**
  * The DrawingSurface class uses all other classes, excluding main, to draw
@@ -6,7 +8,24 @@
  * @author Ophir Peleg
  * @version 05/06/2021
  */
-public class DrawingSurface {
+
+public class DrawingSurface extends PApplet {
 		
+	private ArrayList<Classroom> classroom; 
+	private ArrayList<Zombie> zombies;
+	private Player player;
+	private Map map;
+	
+	public DrawingSurface() {
+		super();
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
 }
 
