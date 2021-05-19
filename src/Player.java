c264b1da75c1f908de6df073c323c39376d9dbb0
Itai Reingold-Nutman <69989734+ireingoldnutman922@users.noreply.github.com>
2021-05-19 11:04:50 -0700

    Revert "WEfixed the movement"
    This reverts commit c06717bafc98beff90e791284db2ecd37fa342f8.

--- src/Player.java
+++ src/Player.java
@@ -9,9 +9,6 @@ import processing.core.PImage;
  */
 public class Player extends Person {
 
-	private int mX; 
-	private int mY; 
-	
 	/**
 	 * Initializes a player to an initial x coordinate, y coordinate, image, and
 	 * visibility of the player.
@@ -23,8 +20,6 @@ public class Player extends Person {
 	 */
 	public Player(int x, int y, PImage img, boolean shown) {
 		super(x, y, img, shown);
-		mX = 0;
-		mY = 0;
 
 	}
 
@@ -38,79 +33,26 @@ public class Player extends Person {
 	public void draw(PApplet marker) {
 		super.draw(marker);
 
-		setX(getX() + mX);
-		setY(getY() + mY);
-		marker.keyReleased();
-		if (marker.key == PApplet.CODED && marker.keyCode == PApplet.UP) {
-			
-				mY = 0;
-		
-		} else if (marker.key == PApplet.CODED && marker.keyCode == PApplet.DOWN) {
-			
-				mY = 0;
-			
-		}
-		if (marker.key == PApplet.CODED && marker.keyCode == PApplet.LEFT) {
-			
-				mX = 0;
-			
-
-		} else if (marker.key == PApplet.CODED && marker.keyCode == PApplet.RIGHT) {
-			
-				mX = 0;
-		
-		}
 		if (marker.keyPressed) {
 			if (marker.key == PApplet.CODED && marker.keyCode == PApplet.UP) {
 				if ((super.getY() > 2)) {
-					//super.setY(super.getY() - 2);
-					mY -= 2;
+					super.setY(super.getY() - 2);
 				}
 			} else if (marker.key == PApplet.CODED && marker.keyCode == PApplet.DOWN) {
 				if (((super.getY() + super.getHeight()) < 600)) {
-					//super.setY(super.getY() + 2);
-					mY += 2;
+					super.setY(super.getY() + 2);
 				}
 			}
 			if (marker.key == PApplet.CODED && marker.keyCode == PApplet.LEFT) {
 				if ((super.getX() > -30)) {
-					//super.setX(super.getX() - 2);
-					mX -= 2;
+					super.setX(super.getX() - 2);
 				}
 
 			} else if (marker.key == PApplet.CODED && marker.keyCode == PApplet.RIGHT) {
 				if (((super.getX() + super.getWidth()) < 830)) {
-					//super.setX(super.getX() + 2);
-					mX += 2;
+					super.setX(super.getX() + 2);
 				}
 			}
 		}
-		
-	
-//		if (marker.key) {
-//			if (marker.key == PApplet.CODED && marker.keyCode == PApplet.UP) {
-//				if ((super.getY() > 2)) {
-//					//super.setY(super.getY() - 2);
-//					mY -= 2;
-//				}
-//			} else if (marker.key == PApplet.CODED && marker.keyCode == PApplet.DOWN) {
-//				if (((super.getY() + super.getHeight()) < 600)) {
-//					//super.setY(super.getY() + 2);
-//					mY += 2;
-//				}
-//			}
-//			if (marker.key == PApplet.CODED && marker.keyCode == PApplet.LEFT) {
-//				if ((super.getX() > -30)) {
-//					//super.setX(super.getX() - 2);
-//					mX -= 2;
-//				}
-//
-//			} else if (marker.key == PApplet.CODED && marker.keyCode == PApplet.RIGHT) {
-//				if (((super.getX() + super.getWidth()) < 830)) {
-//					//super.setX(super.getX() + 2);
-//					mX += 2;
-//				}
-//			}
-//		}
 	}
 }
